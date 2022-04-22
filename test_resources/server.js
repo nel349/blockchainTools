3e38f0b660b70bf9b2e954d005e2b5cfc3f1a5e7
Norman Lopez <noell.lpz@gmail.com>
2022-04-21 17:06:24 -0700

    enable Polygon smart contract deployment.

--- test_resources/server.js
+++ test_resources/server.js
@@ -1,9 +1,12 @@
 const HDWalletProvider = require("hdwallet-provider");
 
-//load single private key as string
-var provider = new HDWalletProvider("afd01d2beeb0db02600fcfee2a85805e6b59a32d6bc89cfa91ef55926e289c3b", "https://ropsten.infura.io/v3/a2293dab520a45fba5a5ab184d14e8ab");
+const rpcURL = 'https://ropsten.infura.io/v3/a2293dab520a45fba5a5ab184d14e8ab'; // ethereum
+const rpcMumbai = 'https://polygon-mumbai.g.alchemy.com/v2/wDKjb83mAC3ok5MgMg1vWvc78NAa59zo'; // Polygon
 
-const rpcURL = 'https://ropsten.infura.io/v3/a2293dab520a45fba5a5ab184d14e8ab';
+//load single private key as string
+var provider = new HDWalletProvider("afd01d2beeb0db02600fcfee2a85805e6b59a32d6bc89cfa91ef55926e289c3b", rpcMumbai);
+
+
 const web3_instance = new Web3(provider);
 
 var transactionHashResult = "";
@@ -16,10 +19,11 @@ var deploy_contract = new web3_instance.eth.Contract(JSON.parse(abi));
 
 let account = '0x33d312c90831F42840E412DC2094a61a99d5Cb57'; 
 
+const gasCost = '40';
 let parameter = {
     from: account,
     gas: web3_instance.utils.toHex(5511498),
-    gasPrice: web3_instance.utils.toHex(web3_instance.utils.toWei('35', 'gwei'))
+    gasPrice: web3_instance.utils.toHex(web3_instance.utils.toWei(gasCost, 'gwei'))
 }
 
 function deployContract() {
@@ -29,6 +33,7 @@ function deployContract() {
 
     deploy_contract.deploy(payload).send(parameter, (_, transactionHash) => {
         console.log('Transaction Hash :', transactionHash);
+        console.log('Gas Cost :', gasCost);
         transactionHashResult = transactionHash;
     }).on('confirmation', () => {}).then((newContractInstance) => {
         console.log('Deployed Contract Address : ', newContractInstance.options.address);
